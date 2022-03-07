39ff6c615d1657d8cc17bc665a2142821f0baf88
moonkanghyuck <moonkanghyuck@gmail.com>
2022-03-07 20:04:44 +0900

    03/07_웹훅 테스트

--- StationerySmash/Assets/Scripts/Battle/Unit/Pencil_State.cs
+++ StationerySmash/Assets/Scripts/Battle/Unit/Pencil_State.cs
@@ -51,6 +51,7 @@ public class Pencil_State : Stationary_UnitState
         }
         if (-stageData.max_Range >= myTrm.position.x)
         {
+            //
             //¿À¸¥ÂÊÀ¸·Î Æ¨°ÜÁ® ³ª¿È
             myTrm.DOJump(new Vector3(myTrm.position.x + 0.2f, 0, myTrm.position.z), 0.3f, 1, 1).OnComplete(() =>
             {
