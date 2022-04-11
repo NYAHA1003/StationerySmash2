2db25c287ebd81fa46c38fdb5fdace491c438d0b
LLL <77712364+elsam0104@users.noreply.github.com>
2022-04-11 23:53:24 +0900

    04/11_어드레서블 재사용성 향상
    enum에 써 놓은 이펙트는 자동으로 로드해줌

--- Assets/Scripts/Battle/Command/EffectCommand.cs
+++ Assets/Scripts/Battle/Command/EffectCommand.cs
@@ -33,9 +33,11 @@ namespace Battle
         }
         public async void AllLoadAssetAsync()
         {
-            await AwitLoadAssetAsync("Attack");
-            await AwitLoadAssetAsync("Stun");
-            await AwitLoadAssetAsync("Slow");
+            for (int i = 0; i < System.Enum.GetValues(typeof(EffectType)).Length; i++)
+            {
+                string s = System.Enum.GetName(typeof(EffectType), i);
+                await AwitLoadAssetAsync(s);
+            }
         }
 
         /// <summary>
