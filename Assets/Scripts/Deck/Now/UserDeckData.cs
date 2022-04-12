5f1fbeecb566d30713b6e37da433a4d06f6ba22e
pilgyu14 <pilgue0822@naver.com>
2022-04-13 00:09:36 +0900

    0413_deckJson

--- Assets/Scripts/Deck/Now/UserDeckData.cs
+++ Assets/Scripts/Deck/Now/UserDeckData.cs
@@ -6,30 +6,32 @@ using UnityEngine;
 public class UserDeckData : MonoBehaviour
 {
     public SaveDataSO saveData;
-    private CardDeckSO cardDeck;
+    public CardDeckSO cardDeck;
 
-    string json; 
     /// <summary>
     /// 유저 세이브 데이터를 Json화 시켜 저장한다
     /// </summary>
     [ContextMenu("DataToJson")]
     public void DataToJson()    
     {
-         json = JsonUtility.ToJson(saveData.userSaveData, false);
+         string json = JsonUtility.ToJson(saveData.userSaveData, true);
         //json을 저장한다 파일로 유니티에셋파일쪽에
         Debug.Log(json);
         string fileName = "saveData";
         string path = Application.dataPath + "/" + fileName + ".json";
+        Debug.Log(path); 
         File.WriteAllText(path, json); 
     }
 
 
     /// <summary>
-    /// json화 시킨 
+    /// json화 시킨 데이터를 데이터로  가져온다
     /// </summary>
     [ContextMenu("JsonToData")]
     public void JsonToData()
     {
-        saveData.userSaveData = JsonUtility.FromJson<UserSaveData>(json);
+        string path = File.ReadAllText(Application.dataPath + "/saveData.json");
+        Debug.Log(path);    
+        saveData.userSaveData = JsonUtility.FromJson<UserSaveData>(path);
     }
 }
