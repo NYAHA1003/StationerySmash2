56752778b5b4c1abce4f432823687edd3a81eabb
sg5404 <77712354+sg5404@users.noreply.github.com>
2022-04-27 11:37:09 +0900

    사운드 설정 작업 완료

--- Assets/Scripts/Main/UI/Setting/SoundManager.cs
+++ Assets/Scripts/Main/UI/Setting/SoundManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
 using Util;
+using TMPro;
 
 namespace Util
 {
@@ -39,6 +40,7 @@ namespace Util
 
 public class SoundManager : MonoBehaviour
 {
+    
 
     [SerializeField]
     private AudioMixer _audioMixer;
@@ -52,20 +54,43 @@ public class SoundManager : MonoBehaviour
     private Slider _bgmSlider;
     [SerializeField]
     private Slider _effectSoundSlider;
+    [SerializeField]
+    private TextMeshProUGUI bgmText;
+    [SerializeField]
+    private TextMeshProUGUI effText;
+    [SerializeField]
+    private Button EffBtn;
+    [SerializeField]
+    private Button BgmBtn;
 
-    int minValue = -40, maxValue = 0;
+    
+    private bool iseffSound = false;
+    private bool isbgmSound = false;
+    private float effValue = 0;
+    private float bgmValue = 0;
 
-    float[] sound = new float[2];
+
+    [SerializeField]
+    int minValue = -40;
+    [SerializeField]
+    int maxValue = 0;
+
+    private void Awake()
+    {
+        SetFuntionSound_UI();
+        BGMSliderText(_bgmSlider.value);
+        EFFSliderText(_effectSoundSlider.value);
+    }
 
     private void OnEnable()
     {
         _audioMixer.GetFloat("BGM", out float bgmValue);
-        _audioMixer.GetFloat("Effect", out float effectValue);
+        _audioMixer.GetFloat("EFF", out float effectValue);
         _bgmSlider.value = bgmValue;
         _effectSoundSlider.value = effectValue; 
     }   
 
-    public void OnSetAudio(int soundType)
+    /*public void OnSetAudio(int soundType)
     {
         sound[0] = _bgmSlider.value;
         sound[1] = _effectSoundSlider.value;
@@ -81,9 +106,86 @@ public class SoundManager : MonoBehaviour
        
         //PlayerPrefs.SetFloat("Bgm",);
         //PlayerPrefs.SetFloat("Effect",);
-    }
+    }*/
     public void PlayEffValum(int sound)
     {
         effSources[sound].Play();
+    }
+
+    private void SetFuntionSound_UI()
+    {
+        ResetFunctionSound_UI();
+        _effectSoundSlider.onValueChanged.AddListener(EFFSliderText); 
+        _bgmSlider.onValueChanged.AddListener(BGMSliderText);
+        BgmBtn.onClick.AddListener(BGMBtnClick);
+        EffBtn.onClick.AddListener(EFFBtnClick);
+    }
+
+    private void ResetFunctionSound_UI()
+    {
+        _bgmSlider.minValue = minValue;
+        _bgmSlider.maxValue = maxValue;
+        _bgmSlider.wholeNumbers = true;
+        _bgmSlider.onValueChanged.RemoveAllListeners();
+
+        _effectSoundSlider.minValue = minValue;
+        _effectSoundSlider.maxValue = maxValue;
+        _effectSoundSlider.wholeNumbers = true;
+        _effectSoundSlider.onValueChanged.RemoveAllListeners();
+
+        BgmBtn.onClick.RemoveAllListeners();
+        EffBtn.onClick.RemoveAllListeners();
+    }
+
+    private void BGMSliderText(float _value)
+    {
+        _audioMixer.SetFloat("BGM", _bgmSlider.value); 
+        _value = (_bgmSlider.value - minValue) / (maxValue - minValue) * 100;
+        bgmText.text = _value.ToString();
+    }
+
+    private void EFFSliderText(float _value)
+    {
+        _audioMixer.SetFloat("EFF", _effectSoundSlider.value);
+        _value = (_effectSoundSlider.value - minValue) / (maxValue - minValue) * 100;
+        effText.text = _value.ToString();
+        
+    }
+
+    private void EFFBtnClick()
+    {
+        if (!iseffSound)
+        {
+            effValue = _effectSoundSlider.value;
+            _effectSoundSlider.value = minValue;
+            _audioMixer.SetFloat("EFF", minValue);
+            _effectSoundSlider.interactable = false;
+            iseffSound = true;
+            return;
+        }
+        _effectSoundSlider.value = effValue;
+        _audioMixer.SetFloat("EFF", effValue);
+        _effectSoundSlider.interactable = true;
+        iseffSound = false;
+        return;
+    }
+
+    private void BGMBtnClick()
+    {
+        
+        if(!isbgmSound)
+        {
+            bgmValue = _bgmSlider.value;
+            _bgmSlider.value = minValue;
+            _audioMixer.SetFloat("BGM", minValue);
+            _bgmSlider.interactable = false;
+            isbgmSound = true;
+            return;
+        }
+        _bgmSlider.value = bgmValue;
+        _audioMixer.SetFloat("BGM", bgmValue);
+        _bgmSlider.interactable = true;
+        isbgmSound = false;
+        return;
     }
 }
