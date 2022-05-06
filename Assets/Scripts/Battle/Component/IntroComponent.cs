dfedd20c518a59f160dbb175e0815afe71e2f336
moonkanghyuck <moonkanghyuck@gmail.com>
2022-05-06 17:17:40 +0900

    05/06_배틀씬 정보창 제작중

--- Assets/Scripts/Battle/Component/IntroComponent.cs
+++ Assets/Scripts/Battle/Component/IntroComponent.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using Utill;
+using Utill.Data;
+using Utill.Load;
 using DG.Tweening;
 using TMPro;
+using Main.Deck;
 
 namespace Battle
 {
@@ -23,9 +25,25 @@ namespace Battle
 		private Transform _playerPencilCase;
 		[SerializeField]
 		private Transform _enemyPencilCase;
+		[SerializeField]
+		private GameObject _introCanvas = null;
+		[SerializeField]
+		private PencilCaseDataSO _playerPencilCaseDataSO = null;
+		[SerializeField]
+		private PencilCaseDataSO _enemyPencilCaseDataSO = null;
+		[SerializeField]
+		private AIDataSO _enemyAIDataSO = null;
+		[SerializeField]
+		private CardDeckSO _inGameCardDataSO = null;
+		[SerializeField]
+		private PencilCaseCard _pencilCaseCard = null;
+		[SerializeField]
+		private List<DeckCard> _deckCards = null;
 
 		//변수
 		private bool _isEndIntro = false;
+		private List<CardData> _playerCardDatas = null;
+		private List<CardData> _enemyCardDatas = null;
 
 		//참조변수 
 		private CameraComponent _cameraComponent = null;
@@ -37,6 +55,30 @@ namespace Battle
 		public void SetInitialization(CameraComponent cameraCommand)
 		{
 			_cameraComponent = cameraCommand;
+			_playerCardDatas = _inGameCardDataSO.cardDatas;
+			_enemyCardDatas = _enemyAIDataSO.cardDataList;
+		}
+
+		/// <summary>
+		/// 덱에 대한 정보를 변경한다
+		/// </summary>
+		public void ShowDeckInfo(TeamType teamType)
+		{
+			int deckCardCount = _deckCards.Count;
+			for (int i = 0; i < deckCardCount; i++)
+			{
+				_deckCards[i].gameObject.SetActive(false);
+			}
+
+			if (teamType == TeamType.MyTeam)
+			{
+				_pencilCaseCard.SetPencilCaseData(_playerPencilCaseDataSO._pencilCaseData);
+				
+			}
+			else
+			{
+				_pencilCaseCard.SetPencilCaseData(_enemyPencilCaseDataSO._pencilCaseData);
+			}
 		}
 
 		/// <summary>
@@ -45,18 +87,32 @@ namespace Battle
 		public IEnumerator SetIntro()
 		{
 			_countText.gameObject.SetActive(true);
+
+			//맵 중앙을 보여준다
 			_countText.text = "3";
 			_cameraComponent.MovingCamera(Vector2.zero, 1f, 0.3f);
-			yield return new WaitForSeconds(0.7f);
+			yield return new WaitForSeconds(1f);
+
+			//내 필통을 보여준다
 			_countText.text = "2";
 			_cameraComponent.MovingCamera(_enemyPencilCase.position, 0.5f, 0.2f);
-			yield return new WaitForSeconds(0.7f);
+			yield return new WaitForSeconds(0.2f);
+			_introCanvas.SetActive(true);
+			yield return new WaitForSeconds(1f);
+			_introCanvas.SetActive(false);
+
+			//상대 필통을 보여준다
 			_countText.text = "1";
 			_cameraComponent.MovingCamera(_playerPencilCase.position, 0.5f, 0.2f);
-			yield return new WaitForSeconds(0.7f);
+			yield return new WaitForSeconds(0.2f);
+			_introCanvas.SetActive(true);
+			yield return new WaitForSeconds(1f);
+			_introCanvas.SetActive(false);
+
+			//다시 내 필통쪽으로 클로즈업한다
 			_countText.text = "GO!";
 			_cameraComponent.MovingCamera(_playerPencilCase.position, 1f, 0.2f);
-			yield return new WaitForSeconds(0.7f);
+			yield return new WaitForSeconds(1f);
 			_countText.gameObject.SetActive(false);
 			_isEndIntro = true;
 		}
