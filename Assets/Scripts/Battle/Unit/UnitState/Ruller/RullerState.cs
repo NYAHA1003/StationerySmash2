2f2fbedafca77b908a809dd5e85755d30238b5b9
sg5404 <77712354+sg5404@users.noreply.github.com>
2022-05-06 16:54:58 +0900

    no message

--- Assets/Scripts/Battle/Unit/UnitState/Ruller/RullerState.cs
+++ Assets/Scripts/Battle/Unit/UnitState/Ruller/RullerState.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Utill.Data;
+using Utill.Tool;
+using DG.Tweening;
 
-public class RullerState : MonoBehaviour
+
+namespace Battle.Units
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public class RullerState : AbstractStateManager
+	{
+		public override void SetState()
+		{
+			//스테이트들을 설정한다
+			_idleState = new RullerIdleState();
+			_waitState = new RullerWaitState();
+			_moveState = new RullerMoveState();
+			_attackState = new RullerAttackState();
+			_damagedState = new RullerDamagedState();
+			_dieState = new RullerDieState();
+			_throwState = new RullerThrowState();
+
+			Reset_CurrentUnitState(_idleState);
+
+			_abstractUnitStateList.Add(_idleState);
+			_abstractUnitStateList.Add(_waitState);
+			_abstractUnitStateList.Add(_moveState);
+			_abstractUnitStateList.Add(_attackState);
+			_abstractUnitStateList.Add(_damagedState);
+			_abstractUnitStateList.Add(_dieState);
+			_abstractUnitStateList.Add(_throwState);
+
+			SetInStateList();
+		}
+
+		public override void Reset_State(Transform myTrm, Transform mySprTrm, Unit myUnit)
+		{
+			base.Reset_State(myTrm, mySprTrm, myUnit);
+			myUnit.SetIsNeverDontThrow(false);
+		}
+	}
+
+	public class RullerIdleState : AbstractIdleState
+	{
+	}
+
+	public class RullerWaitState : AbstractWaitState
+	{
+	}
+
+	public class RullerMoveState : AbstractMoveState
+	{
+	}
+
+	public class RullerAttackState : SeFiceAttackState
+	{
+		
+	}
+
+	public class RullerDamagedState : AbstractDamagedState
+	{
+	}
+
+	public class RullerDieState : AbstractDieState
+	{
+	}
+
+	public class RullerThrowState : AbstractThrowState
+	{
+
+	}
+
 }
