60382d56c1bdab66932f4d4f0901e3f9eb857467
moonkanghyuck <moonkanghyuck@gmail.com>
2022-05-09 22:47:38 +0900

    05/09_돈, 달고나 증가 애니메이션

---
+++ Assets/Scripts/Main/UI/Profile/MoneyComponent.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+using Main.Deck;
+public class MoneyComponent : MonoBehaviour, IUserData
+{
+    [SerializeField]
+    private TextMeshProUGUI _moneyText = null;
+
+    private int _currentMoney = 0;
+    private int _previousMoney = 0;
+
+
+    public void Awake()
+    {
+        SaveManager._instance.SaveData.AddObserver(this);
+        _previousMoney = SaveManager._instance.SaveData.userSaveData._money;
+        SetMoneyText();
+    }
+
+    public void Notify(ref UserSaveData userSaveData)
+    {
+        _currentMoney = userSaveData._money;
+
+        SetMoneyText();
+        StartCoroutine(UpCountingMoney());
+    }
+
+    /// <summary>
+    /// 돈 텍스트 값 수정
+    /// </summary>
+    public void SetMoneyText()
+    {
+        _moneyText.text = _previousMoney.ToString();
+    }
+
+    /// <summary>
+    /// 돈 업데이트
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator UpCountingMoney()
+    {
+        float interval = 0.05f;
+        while (_previousMoney < _currentMoney)
+        {
+            _previousMoney++;
+            SetMoneyText();
+
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+}
