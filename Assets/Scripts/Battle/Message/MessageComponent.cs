d086353e1d0cf04f680eeb01d3859898fe7b2db0
moonkanghyuck <moonkanghyuck@gmail.com>
2022-06-07 00:33:28 +0900

    06/07_메시지 틀 제작

---
+++ Assets/Scripts/Battle/Message/MessageComponent.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageComponent : BattleComponent
+{
+	[SerializeField]
+	private PencilCaseUnit _playerPencilCaseUnit;
+	[SerializeField]
+	private PencilCaseUnit _enemyPencilCaseUnit;
+}
