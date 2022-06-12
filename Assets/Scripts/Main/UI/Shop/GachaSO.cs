0ef3239466f7c9872b7820556c97cd14bf9ac26e
pilgyu14 <pilgue0822@naver.com>
2022-06-13 00:55:00 +0900

    0613_스티커구조짜는중

--- Assets/Scripts/Main/UI/Shop/GachaSO.cs
+++ Assets/Scripts/Main/UI/Shop/GachaSO.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Scriptable Object/Gacha/GachaSO")]
 public class GachaSO : ScriptableObject
 {
+    [Header("뱃지")]
     public float rarePercent; // 레어 확률
     public float epicPercent; // 영웅 확률
+
+    [Header("스티커")]
+    public float rareCount; // 레어 개수
+    public float epicCount; // 영웅 개수
+
+    [Space(10)]
     public int maxAmount; // 구매 한 번에 뽑는 횟수(나올 아이템 개수) 
 }
-
--- Assets/Scripts/Other_Utill/Utill/Data/EventsType.cs
+++ Assets/Scripts/Other_Utill/Utill/Data/EventsType.cs
@@ -27,6 +27,8 @@
         SkipAnimation, // 뽑기 애니메이션 스킵 
         ResetDailyShop, // 일일상점리셋
         ActiveNextBtn, // 뽑기 다음 버튼 활성화 
+        DrawSticker, // 스티커 뽑기 
+        ActiveBadgePanel, // 뱃지 패널 활성화 비활성화 
 
         // 세팅 부분
         ActiveProfileImgPn = 100, // 프로필 이미지 패널 활성화 
