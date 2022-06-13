23711d0852f3456ec274c0cf9f1c83e807f36ab6
sg5404 <77712354+sg5404@users.noreply.github.com>
2022-06-13 22:28:23 +0900

    애니메이션

--- Assets/Scripts/Loading/LoadingUnit.cs
+++ Assets/Scripts/Loading/LoadingUnit.cs
@@ -18,8 +18,7 @@ public class LoadingUnit : MonoBehaviour
     void Update()
     {
         float rotation = rot;
-        rotation = rotateMax * Mathf.Sin(Time.time * rotateSpeed * Time.deltaTime);
-        plus*=-1;
+        rotation = rotateMax * Mathf.Sin(Time.time * rotateSpeed);
         Vector3 vec = new Vector3(0, 0, rotation);
         transform.Rotate(vec);
     }
