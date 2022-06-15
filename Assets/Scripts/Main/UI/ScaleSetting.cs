58a6c96f916015695ff78d4c6823939224777e46
moonkanghyuck <moonkanghyuck@gmail.com>
2022-06-15 22:51:46 +0900

    06/15_해상도 대응

--- Assets/Scripts/Main/UI/ScaleSetting.cs
+++ Assets/Scripts/Main/UI/ScaleSetting.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ScaleSetting : MonoBehaviour
 {
+	[SerializeField]
+	private bool _isScale;
+
 	private void Start()
 	{
-		SettingScale();
+		if(_isScale)
+		{
+			SettingScale();
+		}
+		else
+		{
+			SettingRect();
+		}
 	}
 
 	/// <summary>
 	/// RectTransform을 화면 사이즈에 맞게 변환
 	/// </summary>
-	private void SettingScale()
+	private void SettingRect()
 	{
 		RectTransform rect = GetComponent<RectTransform>();
 		Vector2 size = rect.sizeDelta;
@@ -24,6 +34,40 @@ public class ScaleSetting : MonoBehaviour
 		{
 			size.x = Mathf.Pow(size.x, Mathf.Log(Screen.width, 1920));
 		}
+		if (Screen.height >= 1440)
+		{
+			size.y = Mathf.Pow(size.y, Mathf.Log(Screen.height, 1440));
+		}
+		else
+		{
+			size.y = Mathf.Pow(size.y, Mathf.Log(Screen.height, 1080));
+		}
 		rect.sizeDelta = size;
+	}
+
+
+	/// <summary>
+	/// RectTransform을 화면 사이즈에 맞게 변환
+	/// </summary>
+	private void SettingScale()
+	{
+		Vector3 size = GetComponent<RectTransform>().localScale;
+		if (Screen.width >= 2560)
+		{
+			size.x = Mathf.Pow(size.x, Mathf.Log(Screen.width, 2560));
+		}
+		else
+		{
+			size.x = Mathf.Pow(size.x, Mathf.Log(Screen.width, 1920));
+		}
+		if (Screen.height >= 1440)
+		{
+			size.y = Mathf.Pow(size.y, Mathf.Log(Screen.height, 1440));
+		}
+		else
+		{
+			size.y = Mathf.Pow(size.y, Mathf.Log(Screen.height, 1080));
+		}
+		GetComponent<RectTransform>().localScale = size;
 	}
 }
