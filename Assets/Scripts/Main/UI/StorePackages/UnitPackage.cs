dd34c0b90514616f3d55bd54277d0603e6a9242d
pilgyu14 <pilgue0822@naver.com>
2022-06-16 08:18:14 +0900

    0616_카드팩연출테스트

--- Assets/Scripts/Main/UI/StorePackages/UnitPackage.cs
+++ Assets/Scripts/Main/UI/StorePackages/UnitPackage.cs
@@ -14,42 +14,10 @@ namespace Main.Store
 {
     public class UnitPackage : MonoBehaviour
     {
-        // 임시 보유 데이터 선언
-        private List<bool> _IsHave = new List<bool>();
-        private bool _Pencil = true;
-        private bool _MechaPencil = true;
-        private bool _Eraser = true;
-        private bool _Scissors = false;
-        private bool _Glue = false;
-        private bool _Ruler = false;
-        private bool _Cutterknife = false;
-        private bool _Postit = false;
-        private bool _MechaPencilLead = false;
-        private bool _Pen = false;
-        //임시 보유 데이터 선언 끝
-
-        /// <summary>
-        /// 기본으로 정해져있는 정보
-        /// </summary>
-        int _min = 0;               //팩에서 나오는 최소 갯수
-        int _max = 0;               //팩에서 나오는 최대 갯수
-        int _newPercent = 0;        //신규 캐릭터가 나올 확률(팩당)
-        int _useMoney = 0;          //사용하는 돈 수량
-        int _useDalgona = 0;        //사용하는 달고나 갯수
-        int _unitMaxAmount = 0; // 나올 총 카드 
-        int _newChPercent = 0;      //랜덤 숫자 퍼센트 
-        int _newCharacter = 0;      //새로운 분실물로 뽑힌 유닛 번호
-
-        private List<string> _UnitNameList = new List<string>();
-        private List<int> _NHnum = new List<int>();
-
-        //-----------------------------------------------------------------------------------//
-
         [SerializeField]
         private CardPackSO cardPackSO; // 각 카드팩에 대한 정보 
 
         private CardPackInfo _pickCardPackInfo; // 뽑은 카드팩 정보 
-
 
         [SerializeField]
         private List<int> _curCardAmountList = new List<int>(); // 현재 나눈 뽑을 카드양 리스트 
@@ -65,11 +33,19 @@ namespace Main.Store
 
         [SerializeField]
         private GachaInfo _gachaInfo;
+        [SerializeField]
+        private Sprite _backCardpack;
+        [SerializeField]
+        private GameObject _cardPanel;
+        [SerializeField]
+        private CardMesh _cardMesh; 
 
         [SerializeField]
         private List<GachaCard> gachaCards = new List<GachaCard>(); // 총 아이템개수 
         [SerializeField]
         private GachaCard cardPrefab;
+
+        private bool isNew = false;  // 새로운 카드가 떴냐 
 
         /// <summary>
         /// 랜덤으로 바뀌는 정보
@@ -90,9 +66,25 @@ namespace Main.Store
         #region 세팅관련 
         private void ListenEvent()
         {
-            EventManager.Instance.StartListening(EventsType.DrawCardPack, UpdateCurrentCard);
             EventManager.Instance.StartListening(EventsType.DrawCardPack, (x) => ClickCardPack((int)x));
+            EventManager.Instance.StartListening(EventsType.ActiveAndAnimateCard, ActiveAndAnimateCard); 
         }
+        /// <summary>
+        /// 최대로 나올 뽑기 아이템 생성 
+        /// </summary>
+        private void instantiateItem()
+        {
+            for (int i = 0; i < _gachaInfo.gachaSO.maxAmount; i++)
+            {
+                GachaCard gachaCard = Instantiate(cardPrefab, _gachaInfo.itemsParent.transform);
+                gachaCard.gameObject.SetActive(false);
+                gachaCards.Add(gachaCard);
+            }
+        }
+
+        /// <summary>
+        /// 보유 카드 , 비보유 카드 업데이트 
+        /// </summary>
         private void UpdateCurrentCard()
         {
             SetHaveCard();
@@ -177,11 +169,12 @@ namespace Main.Store
         {
             for (int i = 0; i < _gachaInfo.gachaSO.maxAmount; i++)
             {
-                gachaCards[i].gameObject.SetActive(true);
+                gachaCards[i].gameObject.SetActive(false);
             }
             _curCardAmountList.Clear();
             _curCardType.Clear();
             _quantity = 0;
+            UpdateCurrentCard(); 
         }
 
         /// <summary>
@@ -198,44 +191,8 @@ namespace Main.Store
             //}
             Reset();
             DrawCardPack((PackageType)cardPackType);
-        }
-
-        /// <summary>
-        /// 뽑은 카드 값 설정 ( 스프라이트, 이름 등, 유저데이터 설정 ) 
-        /// </summary>
-        private void SetGachaCard(bool isNew)
-        {
-            for (int i = 0; i < _quantity; i++)
-            {
-                Debug.Log($"{i}번째 카드설정 {_curCardType[i]} {_curCardAmountList[i]} ");
-                UserSaveManagerSO.AddCardData(DeckDataManagerSO.FindStdCardData(_curCardType[i]), _curCardAmountList[i]); // 유저데이터에 카드조각추가 
-                gachaCards[i].gameObject.SetActive(true);
-                gachaCards[i].GetComponent<DeckCard>().SetCard(DeckDataManagerSO.FindHaveCardData(_curCardType[i]));
-                gachaCards[i].ActiveAndAnimate();
-            }
-
-            if (isNew == true)
-            {
-                CardNamingType newCardNamingType = DrawNewCard();
-                UserSaveManagerSO.AddCardData(DeckDataManagerSO.FindStdCardData(newCardNamingType), 1); // 유저데이터 저장 
-                gachaCards[_quantity].gameObject.SetActive(true);
-                gachaCards[_quantity].GetComponent<DeckCard>().SetCard(DeckDataManagerSO.FindHaveCardData(newCardNamingType));
-                gachaCards[_quantity].ActiveAndAnimate();
-            }
-
-        }
-        /// <summary>
-        /// 최대로 나올 뽑기 아이템 생성 
-        /// </summary>
-        private void instantiateItem()
-        {
-            for (int i = 0; i < _gachaInfo.gachaSO.maxAmount; i++)
-            {
-                GachaCard gachaCard = Instantiate(cardPrefab, _gachaInfo.itemsParent.transform);
-                gachaCard.gameObject.SetActive(false);
-                gachaCards.Add(gachaCard);
-            }
-
+            _cardPanel.SetActive(true);
+            _cardMesh.StartMesh(); 
         }
         /// <summary>
         /// 카드팩뽑기시 데이터세팅 
@@ -278,14 +235,11 @@ namespace Main.Store
             }
             // 새 카드 뽑기 여부
             int newCardPercent = Random.Range(1, 101);
-            bool isNewCard = _pickCardPackInfo.newCardPercent <= newCardPercent; // 새로운 분실물이 나오는지
+             isNew = _pickCardPackInfo.newCardPercent >= newCardPercent; // 새로운 분실물이 나오는지
 
-            SetCardSlices( _pickCardPackInfo.amount);
-            SetGachaCard(isNewCard);
-            //if (isNewCard == true)
-            //{
-            //    DrawNewCard();
-            //}
+            SetCardSlices(_pickCardPackInfo.amount);
+            SetGachaCard();
+
         }
 
         /// <summary>
@@ -321,6 +275,46 @@ namespace Main.Store
             }
         }
 
+
+
+        /// <summary>
+        /// 뽑은 카드 값 설정 ( 스프라이트, 이름 등, 유저데이터 설정 ) 
+        /// </summary>
+        private void SetGachaCard()
+        {
+            DeckCard curCard; 
+            for (int i = 0; i < _quantity; i++)
+            {
+                Debug.Log($"{i}번째 카드설정 {_curCardType[i]} {_curCardAmountList[i]} ");
+                UserSaveManagerSO.AddCardData(DeckDataManagerSO.FindStdCardData(_curCardType[i]), _curCardAmountList[i]); // 유저데이터에 카드조각추가 
+                gachaCards[i].gameObject.SetActive(true);
+                curCard = gachaCards[i].GetComponent<DeckCard>();
+                curCard.SetCard(DeckDataManagerSO.FindHaveCardData(_curCardType[i]));
+                gachaCards[i].SetSprite(curCard.CardImage.sprite, _backCardpack, true);
+                gachaCards[i].ActiveAndAnimate();
+            }
+
+            if (isNew == true)
+            {
+                CardNamingType newCardNamingType = DrawNewCard();
+                UserSaveManagerSO.AddCardData(DeckDataManagerSO.FindStdCardData(newCardNamingType), 1); // 유저데이터 저장 
+                gachaCards[_quantity].gameObject.SetActive(true);
+                curCard = gachaCards[_quantity].GetComponent<DeckCard>();
+                curCard.SetCard(DeckDataManagerSO.FindHaveCardData(newCardNamingType));
+                gachaCards[_quantity].SetSprite(curCard.CardImage.sprite, _backCardpack, true);
+                gachaCards[_quantity].ActiveAndAnimate();
+            }
+        }
+
+        private void ActiveAndAnimateCard()
+        {
+            int count = isNew ? _quantity : _quantity - 1; 
+            for(int i = 0; i < count; i++)
+            {
+                gachaCards[i].ActiveAndAnimate(); 
+            }
+        }
+
         /// <summary>
         /// 신규 유닛 획득 함수
         /// </summary>
@@ -332,101 +326,136 @@ namespace Main.Store
             Debug.Log($"새로운 유닛 \"{_newCardNamingType}\"이/가 뽑혔습니다.");
             return _newCardNamingType;
         } //제일 나중에 들어가야 하는 함수임
+
     }
 
-    #region 이전 코드
-    /*
-     *         /// <summary>
-    /// 가지고 있는것과 가지고 있지 않은 학용품을 불러오는 함수
-    /// </summary>
-    private void SetList()
-    {
-        _IsHave.Add(_Pencil);
-        _IsHave.Add(_MechaPencil);
-        _IsHave.Add(_Eraser);
-        _IsHave.Add(_Scissors);
-        _IsHave.Add(_Glue);
-        _IsHave.Add(_Ruler);
-        _IsHave.Add(_Cutterknife);
-        _IsHave.Add(_Postit);
-        _IsHave.Add(_MechaPencilLead);
-        _IsHave.Add(_Pen);
+    
+}
 
-        _UnitNameList.Add("연필");                 //Pencil
-        _UnitNameList.Add("샤프");                 //MechPencil
-        _UnitNameList.Add("지우개");               //Eraser
-        _UnitNameList.Add("가위");                 //Scissors
-        _UnitNameList.Add("풀");                   //Glue
-        _UnitNameList.Add("자");                   //Ruler
-        _UnitNameList.Add("커터칼");               //Cutterknife
-        _UnitNameList.Add("포스트잇");             //Postit
-        _UnitNameList.Add("샤프심");               //MechaPencilLead
-        _UnitNameList.Add("볼펜");                 //Pen
-    }
-    /// <summary>
-    /// 랜덤한 유닛을 골라주는 함수
-    /// </summary>
-    private void Shuffle()
-    {
-        int temp = 0;
-        for (int i = 0; i < 100; i++)
-        {
-            int change = Random.Range(0, _NHnum.Count);
-            int change1 = Random.Range(0, _NHnum.Count);
-            temp = _NHnum[change];
-            _NHnum[change] = _NHnum[change1];
-            _NHnum[change1] = temp;
-        }
-    }
+
+
+#region 이전 코드
+/*
+ *         // 임시 보유 데이터 선언
+    private List<bool> _IsHave = new List<bool>();
+    private bool _Pencil = true;
+    private bool _MechaPencil = true;
+    private bool _Eraser = true;
+    private bool _Scissors = false;
+    private bool _Glue = false;
+    private bool _Ruler = false;
+    private bool _Cutterknife = false;
+    private bool _Postit = false;
+    private bool _MechaPencilLead = false;
+    private bool _Pen = false;
+    //임시 보유 데이터 선언 끝
 
     /// <summary>
-    /// 가지고 있는 유닛 배열로 가져오기 
+    /// 기본으로 정해져있는 정보
     /// </summary>
-    private void HaveUnit()
+    int _min = 0;               //팩에서 나오는 최소 갯수
+    int _max = 0;               //팩에서 나오는 최대 갯수
+    int _newPercent = 0;        //신규 캐릭터가 나올 확률(팩당)
+    int _useMoney = 0;          //사용하는 돈 수량
+    int _useDalgona = 0;        //사용하는 달고나 갯수
+    int _unitMaxAmount = 0; // 나올 총 카드 
+    int _newChPercent = 0;      //랜덤 숫자 퍼센트 
+    int _newCharacter = 0;      //새로운 분실물로 뽑힌 유닛 번호
+
+    private List<string> _UnitNameList = new List<string>();
+    private List<int> _NHnum = new List<int>();
+
+ *         /// <summary>
+/// 가지고 있는것과 가지고 있지 않은 학용품을 불러오는 함수
+/// </summary>
+private void SetList()
+{
+    _IsHave.Add(_Pencil);
+    _IsHave.Add(_MechaPencil);
+    _IsHave.Add(_Eraser);
+    _IsHave.Add(_Scissors);
+    _IsHave.Add(_Glue);
+    _IsHave.Add(_Ruler);
+    _IsHave.Add(_Cutterknife);
+    _IsHave.Add(_Postit);
+    _IsHave.Add(_MechaPencilLead);
+    _IsHave.Add(_Pen);
+
+    _UnitNameList.Add("연필");                 //Pencil
+    _UnitNameList.Add("샤프");                 //MechPencil
+    _UnitNameList.Add("지우개");               //Eraser
+    _UnitNameList.Add("가위");                 //Scissors
+    _UnitNameList.Add("풀");                   //Glue
+    _UnitNameList.Add("자");                   //Ruler
+    _UnitNameList.Add("커터칼");               //Cutterknife
+    _UnitNameList.Add("포스트잇");             //Postit
+    _UnitNameList.Add("샤프심");               //MechaPencilLead
+    _UnitNameList.Add("볼펜");                 //Pen
+}
+/// <summary>
+/// 랜덤한 유닛을 골라주는 함수
+/// </summary>
+private void Shuffle()
+{
+    int temp = 0;
+    for (int i = 0; i < 100; i++)
     {
-        int j = -1;
+        int change = Random.Range(0, _NHnum.Count);
+        int change1 = Random.Range(0, _NHnum.Count);
+        temp = _NHnum[change];
+        _NHnum[change] = _NHnum[change1];
+        _NHnum[change1] = temp;
+    }
+}
 
-        _NHnum.Clear();
+/// <summary>
+/// 가지고 있는 유닛 배열로 가져오기 
+/// </summary>
+private void HaveUnit()
+{
+    int j = -1;
 
-        for (int i = 0; i < _IsHave.Count; i++)
+    _NHnum.Clear();
+
+    for (int i = 0; i < _IsHave.Count; i++)
+    {
+        if (_IsHave[i])
         {
-            if (_IsHave[i])
-            {
-                j++;
-                _NHnum.Add(i);
-            }
+            j++;
+            _NHnum.Add(i);
         }
     }
+}
 
-    /// <summary>
-    /// 가지고 있지 않은 유닛 배열로 가져오기 
-    /// </summary>
-    private void NotHaveUnit()
+/// <summary>
+/// 가지고 있지 않은 유닛 배열로 가져오기 
+/// </summary>
+private void NotHaveUnit()
+{
+    int j = -1;
+
+    _NHnum.Clear();
+
+    for (int i = 0; i < _IsHave.Count; i++)
     {
-        int j = -1;
-
-        _NHnum.Clear();
-
-        for (int i = 0; i < _IsHave.Count; i++)
+        if (!_IsHave[i])
         {
-            if (!_IsHave[i])
-            {
-                j++;
-                _NHnum.Add(i);
-            }
+            j++;
+            _NHnum.Add(i);
         }
     }
+}
 
-    /// <summary>
-    /// 수량이 가지고 있는 캐릭터의 갯수보다 크다면 최댓값을 캐릭터의 갯수로 고정 
-    /// </summary>
-    private void QuantityOverCheck()
+/// <summary>
+/// 수량이 가지고 있는 캐릭터의 갯수보다 크다면 최댓값을 캐릭터의 갯수로 고정 
+/// </summary>
+private void QuantityOverCheck()
+{
+    if (_Quantity > _NHnum.Count)
     {
-        if (_Quantity > _NHnum.Count)
-        {
-            _Quantity = _NHnum.Count;
-        }
+        _Quantity = _NHnum.Count;
     }
+}
 }
 
 /// <summary>
@@ -435,42 +464,37 @@ namespace Main.Store
 /// <param name="cardPackType"></param>
 public void SetPakageSelect(PackageType cardPackType)
 {
-    //switch (cardPackType)
-    //{
-    //    case PackageType.CommonPack:
-    //        _min = 2; _max = 3;
-    //        _newPercent = 1;
-    //        _useMoney = 500;
-    //        _unitMaxAmount = 20;
-    //        _CurrentUnitAmount = _unitMaxAmount;
-    //        break;
-    //    case PackageType.ShinyPack:
-    //        _min = 4; _max = 5;
-    //        _newPercent = 4;
-    //        _useDalgona = 10;
-    //        _unitMaxAmount = 68;
-    //        _CurrentUnitAmount = _unitMaxAmount;
-    //        break;
-    //    case PackageType.LegendaryPack:
-    //        _min = 6; _max = 8;
-    //        _newPercent = 15;
-    //        _useDalgona = 45;
-    //        _unitMaxAmount = 300;
-    //        _CurrentUnitAmount = _unitMaxAmount;
-    //        break;
-    //    default:
-    //        break;
-    //}
-    _Quantity = Random.Range(_min, _max + 1);
-    _newChPercent = Random.Range(0, 100 + 1);
+//switch (cardPackType)
+//{
+//    case PackageType.CommonPack:
+//        _min = 2; _max = 3;
+//        _newPercent = 1;
+//        _useMoney = 500;
+//        _unitMaxAmount = 20;
+//        _CurrentUnitAmount = _unitMaxAmount;
+//        break;
+//    case PackageType.ShinyPack:
+//        _min = 4; _max = 5;
+//        _newPercent = 4;
+//        _useDalgona = 10;
+//        _unitMaxAmount = 68;
+//        _CurrentUnitAmount = _unitMaxAmount;
+//        break;
+//    case PackageType.LegendaryPack:
+//        _min = 6; _max = 8;
+//        _newPercent = 15;
+//        _useDalgona = 45;
+//        _unitMaxAmount = 300;
+//        _CurrentUnitAmount = _unitMaxAmount;
+//        break;
+//    default:
+//        break;
+//}
+_Quantity = Random.Range(_min, _max + 1);
+_newChPercent = Random.Range(0, 100 + 1);
 }
-    */
-    #endregion
-}
-
-
-
-
+*/
+#endregion
 ///// <summary>
 ///// Common패키지를 구매할때 실행하는 함수
 ///// </summary>
