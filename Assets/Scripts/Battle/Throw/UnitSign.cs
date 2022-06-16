c1de51db63d78bf9e8ffa0a3be403f30e6f82a9c
moonkanghyuck <moonkanghyuck@gmail.com>
2022-06-17 08:40:08 +0900

    06/17_던지기바 위치 변경 및 던지기바 스크립트 수정

--- Assets/Scripts/Battle/Throw/UnitSign.cs
+++ Assets/Scripts/Battle/Throw/UnitSign.cs
@@ -18,15 +18,51 @@ public class UnitSign : MonoBehaviour
 	/// 유닛 사인 설정
 	/// </summary>
 	/// <param name="cardData"></param>
-	public void SetSigh(CardData cardData)
+	public void SetSigh(CardData cardData, out int type)
 	{
 		_unitImage.sprite = SkinData.GetSkin(cardData._skinData._skinType);
 		_weight = UnitDataManagerSO.FindStdUnitData(cardData._unitType)._weight;
 		Vector2 rect = _rectTransform.anchoredPosition;
-		rect.x = Mathf.Lerp(-800f, 800f, (float)_weight / 200);
+
+		if(_weight <= 40)
+		{
+			rect.x = -190;
+			type = 0;
+		}
+		else if(_weight <= 80)
+		{
+			rect.x = -105;
+			type = 1;
+		}
+		else if (_weight <= 120)
+		{
+			rect.x = 0;
+			type = 2;
+		}
+		else if (_weight <= 160)
+		{
+			rect.x = 105;
+			type = 3;
+		}
+		else
+		{
+			rect.x = 190;
+			type = 4;
+		}
 
 		_rectTransform.anchoredPosition = rect;
 
 		gameObject.SetActive(true);
+	}
+
+	/// <summary>
+	/// Y 위치 설정
+	/// </summary>
+	/// <param name="value"></param>
+	public void SetY(int value)
+	{
+		Vector2 rect = _rectTransform.anchoredPosition;
+		rect.y = value;
+		_rectTransform.anchoredPosition = rect;
 	}
 }
