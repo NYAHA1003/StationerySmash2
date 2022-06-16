432b4f3ba8a3568dfe17b3f3a5cbf6dd1243b9e3
LLL <77712364+elsam0104@users.noreply.github.com>
2022-06-17 01:58:35 +0900

    0616 버튼eff 추가

--- Assets/Scripts/Main/UI/Button/ButtonIntParam.cs
+++ Assets/Scripts/Main/UI/Button/ButtonIntParam.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Main.Event;
-
+using Main.Setting;
 public class ButtonIntParam : ButtonAgent
 {
     [SerializeField]
     private int[] intParam; 
     public override void Execute()
     {
+        Sound.PlayEff(3);
         for(int i = 0; i < eventTypes.Length; i++)
         {
             EventManager.Instance.TriggerEvent(eventTypes[i], intParam[i]);
