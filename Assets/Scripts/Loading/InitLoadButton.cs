a014c3cba91b5c71a14361fb5cdeda812cc56ae7
bossxz <moonkanghyuck@gmail.com>
2022-06-25 23:22:02 +0900

    06/25_다음 배틀로 이동버튼 제작

--- Assets/Scripts/Loading/InitLoadButton.cs
+++ Assets/Scripts/Loading/InitLoadButton.cs
@@ -2,18 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Utill.Tool;
+using Utill.Data;
+using Main.Event;
+using Main.Setting;
+using System.Linq;
+
 namespace Utill.Load
 {
     public class InitLoadButton : MonoBehaviour
     {
-        [SerializeField]
-        private SceneLoadComponenet _sceneLoadComponenet;
+		private SceneLoadComponenet _sceneLoadComponenet;
         [SerializeField]
         private StageDataListSO _stageDataListSO = null;
         [SerializeField]
         private StageDetailPopupPanel _popupPanel = null;
         [SerializeField]
         private LoadingBattleDataSO loadingBattleDataSO;
+
+		public void Start()
+		{
+            EventManager.Instance.StartListening(EventsType.SetNextStageData, NextBattle);
+		}
+
+		/// <summary>
+		/// 다음 배틀로 이동
+		/// </summary>
+		public void NextBattle()
+        {
+            var last = System.Enum.GetValues(typeof(BattleStageType)).Cast<BattleStageType>().Last();
+            if (AIAndStageData.Instance._currentStageDatas._stageType == last)
+			{
+                return;
+			}
+            var next = AIAndStageData.Instance._currentStageDatas._stageType + 1;
+            loadingBattleDataSO.SetCurrentIndex(next);
+            var currentData = loadingBattleDataSO.CurrentStageData;
+            PencilCaseDataManagerSO.SetEnemyPencilCaseData(currentData);
+            AIAndStageData.Instance.SetAIData(currentData);
+            AIAndStageData.Instance._currentStageDatas = _stageDataListSO.stageDatas.Find(x => x._stageType == next);
+        }
+
         public void InitBattleData(BattleStageType battleStageType)
         {
             loadingBattleDataSO.SetCurrentIndex(battleStageType);
